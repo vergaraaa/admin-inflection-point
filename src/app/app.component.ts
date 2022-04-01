bd0c374c6b146f03b42d513d8aa6311a8dd5bcc1
vergaraaa <edgarvm13@hotmail.com>
2022-04-01 16:05:18 -0600

    created home, login and navbar components with conditional rendering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { NavigationEnd, Router } from '@angular/router'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'api-management';
+  isLogged = true;
+  currentRoute : String = '';
+
+  constructor(private router: Router ) {
+    console.log(router.url);
+
+    router.events.subscribe(Event => {
+        if(Event instanceof NavigationEnd){
+            this.currentRoute = Event.url;
+        }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
