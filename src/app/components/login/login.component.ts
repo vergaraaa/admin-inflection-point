68e53171259f4e1c2c6c72c7992be9c1c7b186f8
vergaraaa <edgarvm13@hotmail.com>
2022-04-06 16:09:53 -0500

    fix background

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -11,23 +11,38 @@ export class LoginComponent implements OnInit {
     constructor(private router: Router) { }
 
     ngOnInit(): void {
-        document.body.style.backgroundAttachment = "fixed";
-        document.body.style.backgroundImage = "url('../../../assets/img/login_background.jpg')";
-        document.body.style.backgroundSize = 'cover';
-        document.body.style.backgroundRepeat = 'no-repeat';
-        document.body.style.overflow = 'hidden';
+        this.putBackground();
     }
 
     googleAuth() {
         console.log('google')
         // TODO: auth with google
+        this.removeBackground();
         this.router.navigate(['/home']);
     }
     
     microsoftAuth() {
         console.log('microsoft')
         // TODO: auth with microsoft
+        this.removeBackground();
         this.router.navigate(['/home']);
     }
+
+    putBackground(){
+        document.body.style.backgroundAttachment = "fixed";
+        document.body.style.backgroundImage = "url('../../../assets/img/login_background.jpg')";
+        document.body.style.backgroundSize = 'cover';
+        document.body.style.backgroundRepeat = 'no-repeat';
+        document.body.style.overflow = 'hidden';
+    }
+    
+    removeBackground(){
+        document.body.style.backgroundAttachment = "initial";
+        document.body.style.backgroundImage = "initial";
+        document.body.style.backgroundSize = 'initial';
+        document.body.style.backgroundRepeat = 'initial';
+        document.body.style.overflow = 'initial';
+    }
+
 
 }
