56bfea0476fa60e3ec4226a14760beaff19da761
EugenioPena <a01177348@itesm.mx>
2022-04-07 15:59:59 -0500

    usuariosView

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { ApiDetailComponent } from './components/api-detail/api-detail.component';
+import { UsersComponent } from './components/users/users.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'api/:id', component: ApiDetailComponent },
+  { path: 'users', component: UsersComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ApiDetailComponent } from './components/api-detail/api-detail.component';
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ApiDetailComponent } from './components/api-detail/api-detail.component
     LoginComponent,
     HomeComponent,
     NavbarComponent,
-    ApiDetailComponent
+    ApiDetailComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
