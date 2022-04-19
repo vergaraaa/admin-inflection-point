4fa2e5249600b9499f552ba486ea7e9a21115a4f
vergaraaa <edgarvm13@hotmail.com>
2022-04-19 18:12:49 -0500

    auth

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -3,21 +3,6 @@ import { GoogleLoginProvider, SocialAuthServiceConfig } from 'angularx-social-lo
 
 // Modulo para providers
 @NgModule({
-  providers: [
-      {
-          provide: 'SocialAuthServiceConfig',
-          useValue: {
-              autoLogin: false,
-              providers: [
-                  {
-                      id: GoogleLoginProvider.PROVIDER_ID,
-                      provider: new GoogleLoginProvider(
-                          '948183837469-mfu3sges0puokgh7q1slh4o9t1s88pco.apps.googleusercontent.com'
-                      )
-                  }
-              ]
-          } as SocialAuthServiceConfig
-      }
-  ],
+  providers: [],
 })
 export class CoreModule {}
