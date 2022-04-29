3eaafc42f4dff20ef35ee01e2718a5996bf3fe1f
ingrid10052 <ingridpartida2001@gmail.com>
2022-04-28 23:41:59 -0500

    API search bar implemented

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api-service.service';
 import { Api } from 'src/app/models/api.model';
+import { elementAt } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +10,23 @@ import { Api } from 'src/app/models/api.model';
 })
 export class HomeComponent implements OnInit {
   apisData: Api[] = [];
-
+  query: string = "";
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
+    this.getApis();
+  }
+  
+  getApis() {
     this.apiService.getApisData().subscribe((data: any) => {
       this.apisData = data;
+    });
+  }
+
+  search() {
+    return this.apisData.filter(api => {
+      return api.name.toLowerCase().includes(this.query.toLowerCase()) 
+          || api.description.toLowerCase().includes(this.query.toLowerCase());
     });
   }
 }
