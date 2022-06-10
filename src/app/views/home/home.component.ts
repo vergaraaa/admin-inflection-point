5b584bc4957d9b0d6b4de37ada46a7b98d244fbf
joelreyna1104 <joelreyna1104@gmail.com>
2022-06-10 16:07:49 -0500

    Agregar sort a todas las tablas / Arreglar todos los searches / Arreglar bug de role_ids moviendose si se buscaba

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -11,6 +11,25 @@ export class HomeComponent implements OnInit {
   apisData: Api[] = [];
   statusData: any = {};
   query: string = '';
+  ascDescBool: boolean = false;
+  tableHeaders: any = [
+    {
+      name: 'API',
+      value: 'name',
+    },
+    {
+      name: 'Descripción',
+      value: 'description',
+    },
+    {
+      name: 'URL',
+      value: 'url',
+    },
+    {
+      name: 'Estado',
+      value: 'status',
+    },
+  ]
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
@@ -21,6 +40,7 @@ export class HomeComponent implements OnInit {
   getApis() {
     this.apiService.getApisData().subscribe((data: any) => {
       this.apisData = data;
+      console.log(this.apisData)
     });
   }
 
@@ -36,7 +56,56 @@ export class HomeComponent implements OnInit {
   getStatus() {
     this.apiService.getStatus().subscribe((data: any) => {
       this.statusData = data;
-      console.log(this.statusData);
     });
+  }
+
+  sort(colName: any, boolean: boolean) {
+    if(colName == 'status'){
+      const statusDataArray = Object.values(this.statusData);
+
+      statusDataArray.forEach((apiStatus: any) => {
+        this.apisData.find(api => {
+          if (api.id == apiStatus.api_id) {
+            api.status = apiStatus.status;
+          }
+        })
+      });
+
+      this.apisData.forEach((api: any) => {
+        if (!api.status) {
+          api.status = false;
+        }
+      }
+      );
+    } 
+
+    if (boolean == true){
+      this.apisData.sort((a: any, b: any): number => {
+        if(typeof a[colName] === 'string') {
+          let aName = a[colName].toLowerCase();
+          let bName = b[colName].toLowerCase();
+          aName = aName.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+          bName = bName.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+          return aName < bName ? 1 : aName > bName ? -1 : 0;
+        } else {
+          return a[colName] < b[colName] ? 1 : a[colName] > b[colName] ? -1 : 0;
+        }
+        })        
+        this.ascDescBool = !this.ascDescBool
+    }
+    else{
+      this.apisData.sort((a: any, b: any): number => {
+        if(typeof a[colName] === 'string') {
+          let aName = a[colName].toLowerCase();
+          let bName = b[colName].toLowerCase();
+          aName = aName.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+          bName = bName.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+          return aName < bName ? -1 : aName > bName ? 1 : 0;
+        } else {
+          return a[colName] < b[colName] ? -1 : a[colName] > b[colName] ? 1 : 0;
+        }
+        })  
+      this.ascDescBool = !this.ascDescBool
+    }
   }
 }
--- src/app/views/users/users.component.ts
+++ src/app/views/users/users.component.ts
@@ -10,11 +10,12 @@ import { User } from 'src/app/models/api.user';
 
 export class UserComponent implements OnInit {
   usersData: User[] = [];
-  roleIds: number[] = [];
   newRole: number = 0;
   idSelectedUser: number = 0;
   indexSelectedUser: number = 0;
+  selectedUser: User = {} as User;
   query: string = "";
+  ascDescBool: boolean = false;
 
   constructor(private userService: UserService) {}
 
@@ -25,18 +26,36 @@ export class UserComponent implements OnInit {
   getUsers(){
     this.userService.getUsers().subscribe((data: any) => {
       this.usersData = data;
-      this.usersData.forEach((user) => this.roleIds.push(user.role_id));
+      console.log(this.usersData)
     });
   }
 
-  onRoleChange(idSelectedUser : number, newRole : number, indexSelectedUser : number) {
-    if(this.idSelectedUser != idSelectedUser){
-      this.roleIds[this.indexSelectedUser] = this.usersData[this.indexSelectedUser].role_id;
-    }
-    this.idSelectedUser = idSelectedUser;
-    this.newRole = newRole;
-    this.indexSelectedUser = indexSelectedUser;
-    this.roleIds[indexSelectedUser] = newRole;
+  // onRoleChange(idSelectedUser : number, newRole : number, indexSelectedUser : number) {
+  //   if(this.idSelectedUser != idSelectedUser){
+  //     this.roleIds[this.indexSelectedUser] = this.usersData[this.indexSelectedUser].role_id;
+  //   }
+  //   this.idSelectedUser = idSelectedUser;
+  //   this.newRole = newRole;
+  //   this.indexSelectedUser = indexSelectedUser;
+  //   this.roleIds[indexSelectedUser] = newRole;
+  // }
+
+  onRoleChange(idSelectedUser: number, newRole: any) {
+    // Set roleChanged to true in user of id idSelectedUser
+    this.usersData.forEach((user) => {
+      if (user.id == idSelectedUser) {
+        console.log(user)
+        if(user.role_id != +newRole.value){
+          user.roleChanged = true;
+          user.newRole = +newRole.value;
+          this.idSelectedUser = idSelectedUser;
+          this.newRole = +newRole.value;
+          this.selectedUser = user;
+        } else{
+          user.roleChanged = false;
+        }
+      }
+    });
   }
 
   editRole(){
@@ -47,8 +66,43 @@ export class UserComponent implements OnInit {
 
   search() {
     return this.usersData.filter(user => {
-      return user.first_name.toLowerCase().includes(this.query.toLowerCase()) 
-          || user.last_name.toLowerCase().includes(this.query.toLowerCase());
+      return user.first_name.toLowerCase().includes(this.query.toLowerCase()) ||
+      user.last_name.toLowerCase().includes(this.query.toLowerCase()) ||
+      user.email.toLowerCase().includes(this.query.toLowerCase()) ||
+      user.first_name.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").includes(this.query.toLowerCase()) ||
+      user.last_name.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").includes(this.query.toLowerCase()) ||
+      user.email.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").includes(this.query.toLowerCase());
     });
+  }
+
+  sort(colName: any, boolean: boolean) {
+    if (boolean == true){
+        this.usersData.sort((a: any, b: any): number => {
+          if(typeof a[colName] === 'string') {
+            let aName = a[colName].toLowerCase();
+            let bName = b[colName].toLowerCase();
+            aName = aName.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+            bName = bName.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+            return aName < bName ? 1 : aName > bName ? -1 : 0;
+          } else {
+            return a[colName] < b[colName] ? 1 : a[colName] > b[colName] ? -1 : 0;
+          }
+          })
+        this.ascDescBool = !this.ascDescBool
+    }
+    else{
+      this.usersData.sort((a: any, b: any): number => {
+        if(typeof a[colName] === 'string') {
+          let aName = a[colName].toLowerCase();
+          let bName = b[colName].toLowerCase();
+          aName = aName.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+          bName = bName.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+          return aName < bName ? -1 : aName > bName ? 1 : 0;
+        } else {
+          return a[colName] < b[colName] ? -1 : a[colName] > b[colName] ? 1 : 0;
+        }
+        })
+        this.ascDescBool = !this.ascDescBool
+    }
   }
 }
