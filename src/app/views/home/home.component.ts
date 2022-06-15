b7190ad3a94f0eea0db9eb90e1ff6e146cc6b47d
ingrid10052 <ingridpartida2001@gmail.com>
2022-06-14 21:00:48 -0500

    Remade Home

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ApiService } from 'src/app/services/api-service.service';
 import { Api } from 'src/app/models/api.model';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import {MatSort} from '@angular/material/sort';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +12,11 @@ import { Api } from 'src/app/models/api.model';
 })
 export class HomeComponent implements OnInit {
   apisData: Api[] = [];
+  displayedColumns = ["name", "description", "url", "status"];
+  dataSource: MatTableDataSource<Api> = new MatTableDataSource;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sortPaginator!: MatSort;
+
   statusData: any = {};
   query: string = '';
   ascDescBool: boolean = false;
@@ -35,12 +43,31 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.getApis();
     this.getStatus();
+    
   }
 
   getApis() {
-    this.apiService.getApisData().subscribe((data: any) => {
-      this.apisData = data;
-      console.log(this.apisData)
+    
+    this.apiService.getApisData().subscribe((dataApi: any) => {
+      this.apiService.getStatus().subscribe((dataStatus: any) => {
+        
+        this.apisData = dataApi;
+        this.statusData = dataStatus;
+        for(let i = 0; i<this.apisData.length; i++){
+          if(this.statusData.hasOwnProperty(this.apisData[i].id)){
+            // this.apisData[i].status = this.statusData[this.apisData[i].id!].status?true:false;
+            this.apisData[i].status = true;
+          }
+          else{
+            this.apisData[i].status = false;
+          }
+          console.log(this.statusData[this.apisData[i].id!]);
+        }
+        // console.log(this.apisData);
+        this.dataSource = new MatTableDataSource(this.apisData);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sortPaginator;
+      });
     });
   }
 
@@ -53,9 +80,14 @@ export class HomeComponent implements OnInit {
     });
   }
 
+  filterData($event : any){
+    this.dataSource.filter = $event.target.value;
+  }
+
   getStatus() {
     this.apiService.getStatus().subscribe((data: any) => {
       this.statusData = data;
+      console.log(data);
     });
   }
 
