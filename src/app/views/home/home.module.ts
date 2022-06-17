5cc6d7315dd26621afa6f275cb8d5ead25dd642a
ingrid10052 <ingridpartida2001@gmail.com>
2022-06-17 02:44:47 -0500

    Fixed layout and validation patterns

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -14,6 +14,7 @@ import {MatSortModule} from '@angular/material/sort';
 import {MatTableModule} from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -28,7 +29,8 @@ import { MatInputModule } from '@angular/material/input';
     MatSortModule,
     MatTableModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatIconModule
   ],
 })
 export class HomeModule {}
